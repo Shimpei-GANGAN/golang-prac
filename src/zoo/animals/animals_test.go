b9c1edcf4ed1afc0ac69a7bd815687399c797e76
gangan <gangan.education@gmail.com>
2022-06-18 16:06:32 +0900

    add: tests.

---
+++ src/zoo/animals/animals_test.go
@@ -0,0 +1,35 @@
+package animals
+
+import (
+	"testing"
+)
+
+// test1: ElephantEeed
+func TestElephantFeed(t *testing.T) {
+	expect := "Grass"
+	actual := ElephantFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
+
+// test2: MonkeyFeed
+func TestMonkeyFeed(t *testing.T) {
+	expect := "Banana"
+	actual := MonkeyFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
+
+// test3: RabbitFeed
+func TestRabbitFeed(t *testing.T) {
+	expect := "Carrot"
+	actual := RabbitFeed()
+
+	if expect != actual {
+		t.Errorf("%s != %s", expect, actual)
+	}
+}
